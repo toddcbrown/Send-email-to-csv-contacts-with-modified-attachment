fb21045296183e3c72bbaefcd9229d4d282b8aa2
Todd Brown <36738690+toddcbrown@users.noreply.github.com>
2022-02-20 18:21:51 +0000

    Try iteration

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
+
 # install yagmail
 import yagmail
 import os 
-sender = 'boor3657@gmail.com'
-receiver = 'xirix46505@sueshaw.com'
-subject = 'This be the subject'
-contents = """
-These are the contents of the email
-"""
+import time 
+import pandas
+df = pandas.read_csv('contacts.csv')
 
 my_secret = os.environ['PASSWORD']
+sender = 'boor3657@gmail.com'
+attachment = 'bill.txt'
+subject = 'Sutro Jersey '
+contents = ''
 
-yag = yagmail.SMTP(user=sender,password=my_secret)
-yag.send(to=receiver,subject=subject,contents=contents)
-print('Email Sent!') 
-
-"""count = 3
-while count > 0:
-  yag.send(to=receiver,subject=(str(count ) + subject),contents=(st(count) + contents))
-  print('Email %s sent'%count)
-  count -= 1 
-"""
+for i in df.index:
+  yag = yagmail.SMTP(user=sender,password=my_secret)
+  contents = 'Hi %s! You owe $%s pay now please!\nThanks!\nTodd\n----'%(df.name[i],df.amount[i])
+  attach = df.filepath[i]#str('attachments/%s'%df.filepath[i])
+  yag.send(df.email[i],subject=subject,contents=contents,attachments=attach)
+  print(df.name[i], ' email sent')
+  
